4499a121fec966dec845cc62149c54acba9b5218
Mark Klagenberg <klags@counterpath.com>
2024-10-18 23:19:49 -0700

    wrokflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/klagster.github.io' : '',
+  assetPrefix: isProd ? '/klagster.github.io/' : '',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
+};
 
 export default nextConfig;
